e5506d292a50d2f4a535481f5265811a4671c5b6
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-09 11:55:12 +0300

    built an jsx html for the select answer

---
+++ components/selectAnswer.js
@@ -0,0 +1,16 @@
+import React from "react"
+function SelectAnswer() {
+    return (
+        <div>
+            <h2>What is your country?</h2>
+            <ul>
+                <li>A</li>
+                <li>B</li>
+                <li>C</li>
+                <li>D</li>
+            </ul>
+            <button>Next</button>
+        </div>
+    )
+}
+export default SelectAnswer;
