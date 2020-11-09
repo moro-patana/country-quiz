55328f17f986d09e039ad7d959985f0cdba12d63
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-09 14:31:06 +0300

    fetch data

--- dist/country-quiz.e31bb0bc.js
+++ dist/country-quiz.e31bb0bc.js
@@ -29809,9 +29809,20 @@ function _getRequireWildcardCache() { if (typeof WeakMap !== "function") return 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } if (obj === null || typeof obj !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
 
 const endPoint = "https://restcountries.eu/rest/v2/name/united";
-console.log(endPoint);
 
 function App() {
+  const [country, setCountry] = (0, _react.useState)([]);
+
+  async function FetchData() {
+    const res = await fetch(endPoint);
+    const data = await res.json();
+    console.log(data);
+    setCountry(data);
+  }
+
+  (0, _react.useEffect)(() => {
+    FetchData();
+  });
   return /*#__PURE__*/_react.default.createElement(_selectAnswer.default, null);
 }
 
--- pages/App.js
+++ pages/App.js
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from "react"
 import SelectAnswer from "../components/selectAnswer"
 const endPoint = "https://restcountries.eu/rest/v2/name/united"
-console.log(endPoint);
 function App() {
+    const [country, setCountry] = useState([])
+    async function FetchData() {
+        const res = await fetch(endPoint)
+        const data = await res.json()
+        console.log(data);
+        setCountry(data)
+    }
+    useEffect(() => {
+        FetchData()
+    })
     return (
         <SelectAnswer/>
     )
