37a4922630c5e16e4717c33fa82e3eafde91b90d
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-09 14:26:53 +0300

    removed fetchData function

--- pages/App.js
+++ pages/App.js
@@ -1,5 +1,7 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import SelectAnswer from "../components/selectAnswer"
+const endPoint = "https://restcountries.eu/rest/v2/name/united"
+console.log(endPoint);
 function App() {
     return (
         <SelectAnswer/>
