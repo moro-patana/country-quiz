2d1bcd619c57b28abf5cf57e7cd5ff5486cb2a4a
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-11-11 16:12:42 +0300

    created variables for the random data

--- pages/App.js
+++ pages/App.js
@@ -1,27 +1,35 @@
 import React, { useEffect, useState } from "react"
 import SelectAnswer from "../components/selectAnswer"
-import Question from "../components/selectAnswer"
-const endPoint = "https://restcountries.eu/rest/v2/name/eesti"
+const endPoint = "https://restcountries.eu/rest/v2/all"
 function App() {
     const [country, setCountry] = useState([])
+    const [dataCapital, setDataCapital] = useState([])
     const fetchData = async() => {
         try {
          const res = await fetch(endPoint);
          const data = await res.json();
-         setCountry(data)
-         console.log(data);
+         const randomData = data[Math.floor(Math.random() * data.length)]
+         const capital1 = data[Math.floor(Math.random() * data.length)]
+         const capital2 = data[Math.floor(Math.random() * data.length)]
+         const capital3 = data[Math.floor(Math.random() * data.length)]
+         const optionCapital = [capital3, capital1, randomData, capital2]
+         setDataCapital(optionCapital)
+         setCountry(randomData)
+      
         } catch(e) {
          console.error(e);
         }
  }
-
+ 
     useEffect(() => {
         fetchData()
     },[])
     return (
         <div>
             <SelectAnswer 
+            fetchData={fetchData}
             country={country}
+            dataCapital={dataCapital}
             />
         </div>
     )
